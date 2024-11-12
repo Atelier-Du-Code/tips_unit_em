5f9669af5bb6bcb3eb6b5b059930abf6cd4a36e2
Manon Avaullée <avaullee.manon@gmail.com>
2024-11-12 08:53:25 +0100

    feat/logic and interface

--- unit_em/src/App.js
+++ unit_em/src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import { Slider } from '@mui/material';
 import './App.css';
 
+function valuetext(value) {
+  return `${value}`;
+}
+
 function App() {
+  const [fontSize, setFontSize] = useState(16);  // Valeur initiale du slider
+  const [fontSizeChildren, setFontSizeChildren] = useState(1);
+  
+  const handleSliderChange = (event, newValue) => {
+    setFontSize(newValue);  // Met à jour l'état à chaque changement de valeur
+  };
+  
+  const handleSliderChildrenChange = (event, newValue) => {
+    setFontSizeChildren(newValue);  // Met à jour l'état à chaque changement de valeur
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='general'>
+      <div className="parent" style={{ fontSize: `${fontSize}px` }}>
+        <h1 className="enfant"  style={{ fontSize: `${fontSizeChildren}em` }}>Texte enfant</h1>
+      </div>
+
+      <div className='param'>
+        
+      <div className="slider">
+        <h2>Taille de la police du parent (px)</h2>
+        <Slider          
+          aria-label="Small steps"
+          value={fontSize}  
+          onChange={handleSliderChange}
+          getAriaValueText={valuetext}
+          step={2}
+          marks
+          min={5}
+          max={25}
+          valueLabelDisplay="auto"
+        />
+      </div>
+
+      <div className="slider">
+        <h2>Taille de la police de l'enfant (em)</h2>
+        <h3>taille de police du parent ( {fontSize}px ) x taille de la police de l'enfant ( {fontSizeChildren}px ) <br/>= taille de la police effective ( {fontSize*fontSizeChildren}px )</h3>
+        <Slider          
+          aria-label="Small steps"
+          value={fontSizeChildren}  
+          onChange={handleSliderChildrenChange}
+          getAriaValueText={valuetext}
+          step={1}
+          marks
+          min={1}
+          max={4}
+          valueLabelDisplay="auto"
+        />
+      </div>
+
+      </div>
+
     </div>
   );
 }
